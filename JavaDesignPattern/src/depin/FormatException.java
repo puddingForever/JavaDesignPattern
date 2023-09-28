6329f335e0943620481712a7235892ffef861771
jieun <developerjieun@gmail.com>
2023-09-28 20:08:31 +0900

    Dependency Inversion Principle:의존관계 역전원칙 - Instead of instantiating dependencies ourselves, let somebody(interface -> parameter) give us the dependencies -의존 관계를 맺을 떄, 객체를 직접적으로 부르지않고 변화하지 않는 것에 의존하는 것 - 객체를 바로 부르는 경우 수정사항이 생길 떄 객체 자체를 변경해야하는 번거로움이 있음. 따라서 메소드에 사용하려는 객체를 직접적으로 생성하지 않고 , 인터페이스를 이용하여 객체를 생성, 파라메터 값으로 던져줘서 사용하는 클라스가 직접 객체를 부르도록 한다.

---
+++ JavaDesignPattern/src/depin/FormatException.java
@@ -0,0 +1,11 @@
+package depin;
+
+import java.io.IOException;
+
+//Thrown by formatter
+public class FormatException extends IOException {
+	
+	public FormatException(Exception cause) {
+		super(cause);
+	}
+}
