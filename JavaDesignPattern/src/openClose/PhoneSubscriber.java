18fd3b2d8ffb804415e0758badccde845d2f61ea
jieun <developerjieun@gmail.com>
2023-09-28 14:43:36 +0900

    Open-closed principle (개방폐쇄원칙) -Software entities(classes,modules,functions,etc) should be open for extention, but closed for modification -소프트웨어 개체(클래스,모듈,함수 등등)는 확장에 대해 열려 있어야하고 수정에 대해서는 닫혀 있어야한다 - Subscriber(Base class:should not be modified) <-- ISPSubscriber,PhoneSubscriber(Derived Class : Open for extension from overriding) -Subscriber class is abstract class that should be not used if the abstract method is not overriden . I made the calculateBill() as the abstract method that should be overriden when other methods arent and cant be modified ! This principal is open-closed princial

---
+++ JavaDesignPattern/src/openClose/PhoneSubscriber.java
@@ -0,0 +1,18 @@
+package openClose;
+
+import java.util.List;
+
+//핸드폰 사용자 계정 클라스 
+public class PhoneSubscriber extends Subscriber{
+
+
+	//open for modification 
+	@Override
+    public double calculateBill() {
+        List<CallHistory.Call> sessions = CallHistory.getCurrentCalls(subscriberId);
+        long totalDuration = sessions.stream().mapToLong(CallHistory.Call::getDuration).sum();
+        return totalDuration*baseRate/100;
+    }
+
+    
+}
