161c8726302c242fd314b225fe7dc89515ff8a38
jieun <developerjieun@gmail.com>
2023-09-28 16:10:56 +0900

    liskov substitution principle(리스코프 치환 원칙) -We should be to substitute base class objects with child class objects & this should not alter behavior /characteristics of program -상위타입의 객체를 하위 타입의 객체로 치환해도 상위 타입을 사용하는 프로그램은 정상적으로 작동해야한다.
    -Square가 Rectangle과 같은 방식으로 width*height를 쓰기 떄문에 Rectangle를 상속받게 된다면,
    Square의 생성자에 있는 setSide가 width와 height를 같은 값으로 바꾸기 떄문에 ,assert
    renctangle.getHeight() == 20 , assert rectangle.getWidth() == 30 를 실행시
    오류가 나게 된다. 왜냐하면 Square의 setSide()메소드에 의해 같은 값으로 변경되기 떄문이다.
    따라서 liskov 치환 방식에 에러가 나게 된다.
    (하위타입 객체(rectangle)로 치환했으나 , 상위타입(square)는 에러가 남)
    따라서 코드를 줄이고 싶다면 차라리 computeArea()가지고 있는 추상메소드를 가질 수 있는 추상클라스 혹은 인터페이스를
    만들어서 사용한다

---
+++ JavaDesignPattern/src/liskov/Shape.java
@@ -0,0 +1,7 @@
+package liskov;
+
+public interface Shape {
+	
+	public int computeArea();
+
+}
---
+++ JavaDesignPattern/src/liskov/Square.java
@@ -0,0 +1,24 @@
+package liskov;
+
+//정사각형 
+public class Square implements Shape {
+	
+	private int side;
+	
+	public Square(int side) {
+		this.side = side;
+	}
+
+	public void setSide(int side) {
+		this.side = side;
+	}
+	
+	public int getSide() {
+		return side;
+	}
+	
+	@Override
+	public int computeArea() {
+		return side*side;
+	}
+}
